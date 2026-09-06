62b969c5dac73cbe719f654aefaae2547355ac3a
Uday Manideep <udaybindu2003@gmail.com>
2026-09-06 20:09:46 +0000

    Fix lint: use single quotes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
-import MovieList from "./components/MovieList";
-import MovieDetails from "./components/MovieDetails";
-import "./App.css";
+import React, { useState } from 'react';
+import MovieList from './components/MovieList';
+import MovieDetails from './components/MovieDetails';
+import './App.css';
 
 export default function App() {
   const [selectedMovie, setSelectedMovie] = useState(null);
